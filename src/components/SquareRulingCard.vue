f4745cc1588a7286c2ec802077049fcafaf5724d
Luis Maldonado <luismaldonado@thrownex.com>
2022-05-14 14:39:09 -0400

    UI for grid view desktop finished

--- src/components/SquareRulingCard.vue
+++ src/components/SquareRulingCard.vue
@@ -15,10 +15,13 @@ const backgroundImage = computed(() => {
 </script>
 
 <template>
-  <div class="square-ruling-card" :style="backgroundImage">
-    <div class="rectangle" style="">
-      <div class="column" style="padding-left: 10%; padding-right: 10%">
-        <div class="row name" style="">
+  <div class="square-ruling-card row" :style="backgroundImage">
+    <div class="row ruling-card-thumb">
+      <img src=" src/assets/img/thumbs-up.svg" alt="thumbs up" />
+    </div>
+    <div class="column rectangle">
+      <div class="column square-ruling-card-info">
+        <div class="row name">
           {{ cutString(props.name, 22) }}
         </div>
         <div class="row description">
@@ -27,7 +30,7 @@ const backgroundImage = computed(() => {
         <div class="row date">
           1 month ago in Entertainment
         </div>
-        <div class="row" style="justify-content: right;">
+        <div class="row square-ruling-card-actions">
           <div class="column">
             <button class="thumbs-up">
               <img src="src/assets/img/thumbs-up.svg" alt="thumbs up">
@@ -43,21 +46,41 @@ const backgroundImage = computed(() => {
           </div>
         </div>
       </div>
-    </div>
-    <div class=" row thumbs-gauge" style="height: 36px;">
-      <div class="column thumbs-gauge--up">
-        <img src=" src/assets/img/thumbs-up.svg" style="margin-left: 16px;">
-        <span class="numbers">25.5%</span>
-      </div>
-      <div class="column thumbs-gauge--down">
-        <img src="src/assets/img/thumbs-down.svg" style="float: right; margin-right: 16px;">
-        <span class="numbers" style="float: right;">74.5%</span>
+      <div class=" row thumbs-gauge">
+        <div class="row thumbs-gauge--up">
+          <img class="thumbs-gauge--up-icon" src=" src/assets/img/thumbs-up.svg">
+          <span class="numbers">25.5%</span>
+        </div>
+        <div class="row thumbs-gauge--down">
+          <span class="numbers">74.5%</span>
+          <img class="thumbs-gauge--down-icon" src="src/assets/img/thumbs-down.svg">
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.thumbs-gauge--up-icon {
+  margin-left: 16px;
+  margin-right: 8px;
+}
+
+.thumbs-gauge--down-icon {
+  float: right;
+  margin-right: 16px;
+  margin-left: 8px;
+}
+.ruling-card-thumb {
+  background: #fbbd4a;
+  height: 30px;
+  width: 30px;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 50%;
+  z-index: 1;
+}
 .numbers {
   width: 51px;
   height: 22px;
@@ -76,12 +99,16 @@ const backgroundImage = computed(() => {
   background-color: rgba(60, 187, 180, 0.6);
   margin-right: auto;
   width: 100%;
+  align-items: center;
+  display: flex;
 }
 
 .thumbs-gauge--down {
   background-color: rgba(249, 173, 29, 0.6);
   margin-left: auto;
   width: 100%;
+  align-items: center;
+  justify-content: end;
 }
 .row {
   display: flex;
@@ -94,6 +121,14 @@ const backgroundImage = computed(() => {
   height: 348px;
   width: 348px;
   margin-bottom: 27px;
+  position: relative;
+}
+
+.thumbs-gauge {
+  height: 36px;
+  width: 100%;
+  bottom: 0;
+  position: absolute;
 }
 
 .vote-now {
@@ -130,6 +165,9 @@ const backgroundImage = computed(() => {
   top: 0%;
   bottom: 0%;
   background: rgba(60, 187, 180, 0.8);
+  margin-right: 8px;
+  margin-top: 4px;
+  border: 2px solid #ffffff;
 }
 .thumbs-down {
   height: 30px;
@@ -139,6 +177,9 @@ const backgroundImage = computed(() => {
   top: 0%;
   bottom: 0%;
   background: #fbbd4a;
+  margin-right: 8px;
+  margin-top: 4px;
+  border: none;
 }
 
 .date {
@@ -156,6 +197,7 @@ const backgroundImage = computed(() => {
   justify-content: right;
 
   color: #ffffff;
+  margin-top: 16px;
 }
 
 .name {
@@ -183,8 +225,20 @@ const backgroundImage = computed(() => {
   color: #ffffff;
 }
 
+.square-ruling-card-info {
+  padding-left: 10%;
+  padding-right: 10%;
+}
+
+.square-ruling-card-actions {
+  justify-content: right;
+  margin-top: 16px;
+}
+
 .rectangle {
-  padding-top: 147px;
+  position: absolute;
+  bottom: 0;
+  height: 70%;
   background: linear-gradient(
     180deg,
     rgba(0, 0, 0, 0.0001) 0%,
